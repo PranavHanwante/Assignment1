1ac05eb7061165e4e16efeb8bd5cdd7866cbdc6d
Pranav <hanwantepranav@gmail.com>
2020-09-23 12:17:05 +0530

    This is in  beta branch, separated the method with 1st 3 steps togeather and price in antoher method

--- src/test/java/firstGroup/Assignment1Copy.java
+++ src/test/java/firstGroup/Assignment1Copy.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -16,40 +15,46 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class Assignment1 {
+public class Assignment1Copy {
 
-    WebDriver driver;
-    @Test
-    public void test1(){
-     startOperationBegin();
-     System.out.println("test1 = ");
-     Boolean b =   mainOperation();
-     Assert.assertTrue(b);
-    }
-    @Test
-    public void test2(){
-        startOperationBegin();
-        System.out.println("test2 = ");
-        Boolean b =   mainOperation();
-        Assert.assertTrue(b);
-    }
 
-    public void doStartingOpeartion(String product,String sortItem){
 
-        product = product.trim();
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"\\src\\test\\resource\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-         driver=new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get("https://www.flipkart.com/");
+
+        @Test
+        public void test1(){
+
+            Boolean b =  getDetails();
+            System.out.println("test1 = ");
+            Assert.assertTrue(b);
+        }
+        @Test
+        public void test2(){
+
+            Boolean b =  getDetails();
+            System.out.println("test2 = ");
+            Assert.assertTrue(b);
+        }
+
+        public void doStartingOpeartion(){
+
+        }
+
+        public  Boolean performOpeation(String product,String sortItem){
+
+            product = product.trim();
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"\\src\\test\\resource\\chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--incognito");
+            WebDriver driver=new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            driver.get("https://www.flipkart.com/");
 //**********************Pop up cancel*********************
 
-        Assignment1Definition obj1 = new Assignment1Definition(driver);
-        if (obj1.popUpCancel().isDisplayed()){
-            obj1.popUpCancel().click();
-        }
+            Assignment1Definition obj1 = new Assignment1Definition(driver);
+            if (obj1.popUpCancel().isDisplayed()){
+                obj1.popUpCancel().click();
+            }
 
 //Pop up cancel
 //        if(driver.findElement(By.xpath("//div[@class='mCRfo9']/div/div/button")).isDisplayed()){
@@ -61,53 +66,50 @@ public class Assignment1 {
 //        driver.findElement(By.xpath("//form/div/div/input")).sendKeys(product);
 //        driver.findElement(By.xpath("//button[@class='vh79eN']")).click();
 
-        obj1.setShoes().sendKeys(product);
-        obj1.clickSearch().click();
+            obj1.setShoes().sendKeys(product);
+            obj1.clickSearch().click();
 
 
 
 //****************Sorting low to high filter***************************
-        List<WebElement> total = driver.findElements(By.xpath("//div[@class='_3ywJNQ']/div"));
-        Iterator<WebElement> itTotal = total.iterator();
-        ArrayList a1 = new ArrayList();
-        ArrayList a2 = new ArrayList();
-        int count=0;int position=0;
-        while(itTotal.hasNext()){
-            count++;
-            String temp=itTotal.next().getText();
-            temp=temp.trim();
-            if(sortItem.equalsIgnoreCase(temp)){
-                position=count;
-                driver.findElement(By.xpath("//div[@class='_3ywJNQ']/div["+position+"]")).click();
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            List<WebElement> total = driver.findElements(By.xpath("//div[@class='_3ywJNQ']/div"));
+            Iterator<WebElement> itTotal = total.iterator();
+            ArrayList a1 = new ArrayList();
+            ArrayList a2 = new ArrayList();
+            int count=0;int position=0;
+            while(itTotal.hasNext()){
+                count++;
+                String temp=itTotal.next().getText();
+                temp=temp.trim();
+                if(sortItem.equalsIgnoreCase(temp)){
+                    position=count;
+                    driver.findElement(By.xpath("//div[@class='_3ywJNQ']/div["+position+"]")).click();
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    WebDriverWait wait = new WebDriverWait(driver, 10);
+                    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='_1vC4OE'])[1]")));
+
+
+
                 }
-                WebDriverWait wait = new WebDriverWait(driver, 10);
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='_1vC4OE'])[1]")));
-
-            }
 //            System.out.println("temp = "+temp);
-        }
-        System.out.println("product = "+product);
-        System.out.println("position = "+position);
-        System.out.println(sortItem);
-
-    }
+            }
+            System.out.println("product = "+product);
+            System.out.println("position = "+position);
+            System.out.println(sortItem);
 
 
+            //////////////////////////******************************//////////////////////////////
 
-
-    public  Boolean mainOperation(){
-
-
-   //////////////////////////******************************//////////////////////////////
-
-        //**********price xpath, checking price is in asecding order*****************
-        Assignment1SortPrice obj = new Assignment1SortPrice(driver);
-        Boolean orderBoolean;
-        obj.getBoolean();
+            //**********price xpath, checking price is in asecding order*****************
+            Assignment1SortPrice obj = new Assignment1SortPrice(driver);
+            Boolean orderBoolean;
+            obj.getBoolean();
 //        Assert.assertTrue(orderBoolean);
 
 
@@ -133,44 +135,44 @@ public class Assignment1 {
 
 
 //****pagination page 2***********
-        Assignment1Definition obj1 = new Assignment1Definition(driver);
 
-        obj1.clickPagination().click();
+
+            obj1.clickPagination().click();
 
 //driver.findElement(By.xpath("//div[@class='_2zg3yZ']/nav/a[2]")).click();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebDriverWait wait = new WebDriverWait(driver, 10);
 //        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='_1vC4OE'])[1]")));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='_1vC4OE'])[1]")));
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='_1vC4OE'])[1]")));
 
 
 
-        orderBoolean =obj.getBoolean();
-        System.out.println("orderBoolean = "+orderBoolean);
+            orderBoolean =obj.getBoolean();
+            System.out.println("orderBoolean = "+orderBoolean);
 
 //        Assert.assertTrue(orderBoolean);
 
-return orderBoolean;
-    }
+            return orderBoolean;
+        }
 
-    public void startOperationBegin(){
+        public Boolean getDetails(){
 
-        ReadExcelFile configuration = new ReadExcelFile(System.getProperty("user.dir") +"\\src\\test\\resource\\assignment_test.xlsx");
-        //  int rows = configuration.getRowCount(0);
-        Object[][]signin_credentials = new Object[1][2];
-        String product =configuration.getData(0, 0, 0);
-        String sortItem= configuration.getData(0, 0, 1);
+            ReadExcelFile configuration = new ReadExcelFile(System.getProperty("user.dir") +"\\src\\test\\resource\\assignment_test.xlsx");
+            //  int rows = configuration.getRowCount(0);
+            Object[][]signin_credentials = new Object[1][2];
+            String product =configuration.getData(0, 0, 0);
+            String sortItem= configuration.getData(0, 0, 1);
 
-       doStartingOpeartion(product,sortItem);
+            return performOpeation(product,sortItem);
 
 
-    }
+        }
 
 
 
@@ -313,4 +315,17 @@ return orderBoolean;
 //    }
 
 
-}
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
